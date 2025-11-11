70078c3513becf65fe253093840f81281ccfa6c6
Esa <esaruhulla01@gmail.com>
2025-11-11 19:23:25 +0600

    env file added

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 3000;
+
+const port = process.env.PORT || 3000;
+const user = process.env.DB_USER;
+const pass = process.env.DB_PASS;
+
+
 
 // ✅ Middleware
 app.use(cors());
@@ -11,7 +18,8 @@ app.use(express.json());
 //Habit-Tracker
 //pl82L5iFFa98mD90
 
-const uri = "mongodb+srv://Habit-Tracker:pl82L5iFFa98mD90@cluster0.4u6lkgk.mongodb.net/?appName=Cluster0";
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4u6lkgk.mongodb.net/?appName=Cluster0`;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -32,6 +40,9 @@ async function run() {
     await client.connect();
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    // const db = client.db(process.env.DB_NAME); //databse select
+    // const usersCollection = db.collection('users'); //collection select
   }
   finally {
 
